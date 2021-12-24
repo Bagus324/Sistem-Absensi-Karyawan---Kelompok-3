1149e5f7cfa4f0e0cbfdf0ce3f2a4e9114117640
Bagus324 <bagusdarmawan654@gmail.comm>
2021-12-24 09:17:40 +0700

    Penambahan TextBox Username pada Form Login

--- Sistem_Absensi_Karyawan/Form1.Designer.vb
+++ Sistem_Absensi_Karyawan/Form1.Designer.vb
@@ -22,10 +22,40 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.TBUname = New System.Windows.Forms.TextBox()
+        Me.LBLUname = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'TBUname
+        '
+        Me.TBUname.Location = New System.Drawing.Point(150, 64)
+        Me.TBUname.Name = "TBUname"
+        Me.TBUname.Size = New System.Drawing.Size(150, 20)
+        Me.TBUname.TabIndex = 0
+        '
+        'LBLUname
+        '
+        Me.LBLUname.AutoSize = True
+        Me.LBLUname.Location = New System.Drawing.Point(71, 67)
+        Me.LBLUname.Name = "LBLUname"
+        Me.LBLUname.Size = New System.Drawing.Size(55, 13)
+        Me.LBLUname.TabIndex = 1
+        Me.LBLUname.Text = "Username"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.LBLUname)
+        Me.Controls.Add(Me.TBUname)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents TBUname As TextBox
+    Friend WithEvents LBLUname As Label
 End Class
