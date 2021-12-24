2a85a1a630fcd8f2740c100a4c39039a5c9e3449
Bagus324 <bagusdarmawan654@gmail.comm>
2021-12-24 10:14:56 +0700

    adding NIK

--- Sistem_Absensi_Karyawan/Login.Designer.vb
+++ Sistem_Absensi_Karyawan/Login.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Login
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -23,7 +23,7 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.TBUname = New System.Windows.Forms.TextBox()
-        Me.LBLUname = New System.Windows.Forms.Label()
+        Me.LBLNIK = New System.Windows.Forms.Label()
         Me.SuspendLayout()
         '
         'TBUname
@@ -33,29 +33,29 @@ Partial Class Form1
         Me.TBUname.Size = New System.Drawing.Size(150, 20)
         Me.TBUname.TabIndex = 0
         '
-        'LBLUname
+        'LBLNIK
         '
-        Me.LBLUname.AutoSize = True
-        Me.LBLUname.Location = New System.Drawing.Point(71, 67)
-        Me.LBLUname.Name = "LBLUname"
-        Me.LBLUname.Size = New System.Drawing.Size(55, 13)
-        Me.LBLUname.TabIndex = 1
-        Me.LBLUname.Text = "Username"
+        Me.LBLNIK.AutoSize = True
+        Me.LBLNIK.Location = New System.Drawing.Point(71, 67)
+        Me.LBLNIK.Name = "LBLNIK"
+        Me.LBLNIK.Size = New System.Drawing.Size(25, 13)
+        Me.LBLNIK.TabIndex = 1
+        Me.LBLNIK.Text = "NIK"
         '
-        'Form1
+        'Login
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(800, 450)
-        Me.Controls.Add(Me.LBLUname)
+        Me.Controls.Add(Me.LBLNIK)
         Me.Controls.Add(Me.TBUname)
-        Me.Name = "Form1"
-        Me.Text = "Form1"
+        Me.Name = "Login"
+        Me.Text = "Login"
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
     Friend WithEvents TBUname As TextBox
-    Friend WithEvents LBLUname As Label
+    Friend WithEvents LBLNIK As Label
 End Class
