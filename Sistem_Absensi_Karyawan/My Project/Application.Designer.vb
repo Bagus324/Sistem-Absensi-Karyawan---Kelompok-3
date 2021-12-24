588e1a6e9d036e5bea63df403921123e354d4677
Bagus324 <bagusdarmawan654@gmail.comm>
2021-12-24 09:36:06 +0700

    Edit Readme

--- Sistem_Absensi_Karyawan/My Project/Application.Designer.vb
+++ Sistem_Absensi_Karyawan/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
 
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()> _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Sistem_Absensi_Karyawan.Form1
+            Me.MainForm = Global.Sistem_Absensi_Karyawan.Login
         End Sub
     End Class
 End Namespace
